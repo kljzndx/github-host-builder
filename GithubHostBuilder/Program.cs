af8d0c2c0a0e846b44a8a918a2a0b3720ef0fb52
李继龙 <kljzndx@outlook.com>
2021-03-29 09:22:41 +0800

    完成主要功能编写

--- GithubHostBuilder/Program.cs
+++ GithubHostBuilder/Program.cs
@@ -11,11 +11,64 @@ namespace GithubHostBuilder
     {
         static void Main(string[] args)
         {
-            
+            string[] allHosts = 
+            {
+                "github.com",
+                "gist.github.com",
+                "assets-cdn.github.com",
+                "raw.githubusercontent.com",
+                "cloud.githubusercontent.com",
+                "camo.githubusercontent.com",
+                "avatars0.githubusercontent.com",
+                "avatars1.githubusercontent.com",
+                "avatars2.githubusercontent.com",
+                "avatars3.githubusercontent.com",
+                "avatars4.githubusercontent.com",
+                "avatars5.githubusercontent.com",
+                "avatars6.githubusercontent.com",
+                "avatars7.githubusercontent.com",
+                "avatars8.githubusercontent.com"
+            };
+
+            string[] needCheckHosts = allHosts.Take(4).ToArray();
+            var ipListOfHost = new Dictionary<string, string[]>();
+
+            foreach (var item in needCheckHosts)
+                ipListOfHost.Add(item, GetIpList(item));
+
+            var bastIpList = new List<string>();
+
+            foreach (var item in ipListOfHost)
+            {
+                try
+                {
+                    bastIpList.Add(GetFastestIp(item.Key, item.Value));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
+
+            Console.WriteLine("Begin building");
+
+            var hostsFile = new List<string>();
+            for (int i = 0; i < allHosts.Length; i++)
+            {
+                string bastIp = i < bastIpList.Count ? bastIpList[i] : bastIpList.Last();
+                hostsFile.Add($"{bastIp}    {allHosts[i]}");
+            }
+
+            Console.WriteLine("Complete build");
+            Console.WriteLine();
+            foreach (var item in hostsFile)
+                Console.WriteLine(item);
         }
 
-        static string GetFastestIp(string[] ipList)
+        static string GetFastestIp(string host, string[] ipList)
         {
+            Console.WriteLine($"Testing ip address list of '{host}'");
             var speedOfIp = new Dictionary<string, long>();
             ProgressBar progressBar = new ProgressBar(ipList.Length, "Testing ip address");
 
@@ -33,15 +86,20 @@ namespace GithubHostBuilder
                 }
 
                 if (speeds.Any())
-                    speedOfIp.Add(item, (long) speeds.Average());
+                    speedOfIp.Add(item, (long)speeds.Average());
             }
+
+            progressBar.Dispose();
+            Console.WriteLine();
 
             if (!speedOfIp.Any())
                 throw new Exception("No available ip address");
 
             long fastestSpeed = speedOfIp.Min(i => i.Value);
+            var fastestIp = speedOfIp.First(v => v.Value == fastestSpeed);
 
-            return speedOfIp.First(v => v.Value == fastestSpeed).Key;
+            Console.WriteLine(fastestIp.Key + "    " + fastestIp.Value + "ms");
+            return fastestIp.Key;
         }
 
         static string[] GetIpList(string host)
